b8ba5c7c27afff873eb8bce73b751ffd3b011f68
PauloR2021 <paulotrindadeoficial@gmail.com>
2025-06-05 18:39:55 -0300

    Criando arquivo .env | .gitignore | criando conexao com o banco de dados e instalando bibliotecas

---
+++ src/coleta_dados.py
@@ -0,0 +1,11 @@
+""""
+Desenvolvido por: Paulo Ricardo Soares
+Data: 05/06/2025
+Hora: 18:14
+
+Criando a coleta de dados utilizando a bilbioteca Pandas
+Este arquivo tem como função coletar dados das cidades.
+"""
+
+import pandas as pd
+
